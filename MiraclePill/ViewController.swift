0b08aceed11d041936823958f791b3e8b27a2f5c
mucsi.tamas <mucsi.tamas@wwdh.hu>
2017-08-22 16:55:22 +0200

    init commit

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Mucsi Tamás on 2017. 08. 22..
+//  Copyright © 2017. Mucsi Tamás. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
